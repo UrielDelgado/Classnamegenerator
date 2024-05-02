ea6a710fb3b37366674c41fc91cce5906cef8c22
UrielDelgado <delgadouriel83@gmail.com>
2024-05-02 10:04:16 -0700

    question to names
    changed question to names

--- demo.js
+++ demo.js
@@ -1,21 +1,21 @@
 function generateIcebreaker() {
   const questions = [
-    "What's your favorite way to unwind after a long day of coding?",
-    "What hobby would you get into if time and money weren't an issue?",
-    "What's the last book you read or movie you watched that you really enjoyed?",
-    "If you could visit any country in the world, where would you go and why?",
-    "What's one skill you'd like to develop or hobby you'd like to take up?",
-    "What's the most memorable vacation you've ever had?",
-    "If you could have dinner with any historical figure, who would it be and why?",
-    "What's your favorite season of the year and why?",
-    "What’s your favorite dish to cook or eat?",
-    "If you could instantly become an expert in something, what would it be?",
+    "my goat Daniel !!!",
+    "my goat Rene !!!",
+    "my goat Kaaliyah !!!",
+    "my goat Alfi !!!",
+    "my goat Dante !!!",
+    "my goat Kayla !!!",
+    "my goat Abdi !!!",
+    "my goat Victor !!!",
+    "my goat Kadiatou !!!",
+
   ];
 
   // Randomly select a question
   const index = Math.floor(Math.random() * questions.length);
 
-  document.getElementById("displayQuestion").innerHTML = questions[index];
+  document.getElementById("displayQuestion").innerHTML = "You are " +  questions[index];
   // return questions[index];
 }
 
